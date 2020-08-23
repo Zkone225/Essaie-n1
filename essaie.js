43123b0531a9f2791557a2343c0cd8dfb938038b
Zkone225 <69790618+Zkone225@users.noreply.github.com>
2020-08-23 13:42:30 +0200

    dom.

--- essaie.js
+++ essaie.js
@@ -1,1 +1,4 @@
-alert("ciao");
+let nombre = Number(prompt("inserisci un numero e ti farò la sua tabella delle multiplicazioni"));
+for (let i = 1; i <=10 ; i++) {
+    console.log(nombre + " x " + i + " = " + nombre*i )
+}
